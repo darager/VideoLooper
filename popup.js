c07e6fad2ebfdfddd2b96f5a91c6c0c481a5f6ec
Rager Florian <florianrager90@gmail.com>
2021-11-02 10:35:44 +0100

    Add Button to Play/Pause Video

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 let changeColor = document.getElementById("changeColor");
+let playpauseVideo = document.getElementById("playpauseVideo");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+playpauseVideo.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: startStopVideo,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function startStopVideo() {
+  var video = document.getElementsByTagName("video")[0];
+
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
