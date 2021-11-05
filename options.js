e3d4d887afb079a6bab1df66506cb791ce50ab70
Rager Florian <florianrager90@gmail.com>
2021-11-05 10:10:46 +0100

    Constrain values on Options Page

--- options.js
+++ options.js
@@ -2,6 +2,7 @@ loadValues();
 
 document.getElementById("resetSettings").addEventListener("click", loadValues);
 document.getElementById("saveSettings").addEventListener("click", saveSettings);
+document.getElementById("ensureProperValues").addEventListener("click", ensureProperValues);
 
 var speedValuesInput = document.getElementById("speedValues");
 var moveVideoBySelector = document.getElementById("moveVideoBy");
@@ -17,6 +18,33 @@ function loadValues() {
 }
 
 function ensureProperValues() {
+  moveVideoBySelector.value = constrain(moveVideoBySelector.value, 0.1, null);
+  preferedSpeedSelector.value = constrain(preferedSpeedSelector.value, 0.1, 5);
+  changeSpeedBySelector.value = constrain(changeSpeedBySelector.value, 0.1, null);
+
+  let values = parseSpeedValues(speedValuesInput.value);
+  let constrainedValues = values.map((v) => constrain(v, 0.1, 5));
+  let uniqueValues = [...new Set(constrainedValues)];
+  let sorted = uniqueValues.sort();
+  let valuesText = sorted.join(',');
+  speedValuesInput.value = valuesText;
+}
+
+function constrain(value, min, max) {
+  if(min != null && value < min) value = min;
+  if(max != null && value > max) value = max;
+  return value;
+}
+
+const regexp = /(\d+\.\d+|\d+)/g;
+function parseSpeedValues(string) {
+  var values = [];
+  let matches = [...string.matchAll(regexp)];
+  for (match of matches){
+    let value = match[0];
+    values.push(value);
+  }
+  return values;
 }
 
 function saveSettings() {
