4e69294a803df3590d5a0c561022a18607312e0f
Rager Florian <florianrager90@gmail.com>
2021-11-05 10:27:16 +0100

    Properly constrain settings and store them

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-loadValues();
+const numberExp = /(\d+\.\d+|\d+)/g;
 
 document.getElementById("resetSettings").addEventListener("click", loadValues);
 document.getElementById("saveSettings").addEventListener("click", saveSettings);
@@ -9,42 +9,14 @@ var moveVideoBySelector = document.getElementById("moveVideoBy");
 var changeSpeedBySelector = document.getElementById("changeSpeedBy");
 var preferedSpeedSelector = document.getElementById("preferedSpeed");
 
+loadValues();
+ensureProperValues();
+
 function loadValues() {
-    console.log('getting values');
     getValue("speedValues", (v) => speedValuesInput.value = v);
     getValue("moveVideoBy", (v) => moveVideoBySelector.value = v);
     getValue("changeSpeedBy", (v) => changeSpeedBySelector.value = v);
     getValue("preferedSpeed", (v) => preferedSpeedSelector.value = v);
-}
-
-function ensureProperValues() {
-  moveVideoBySelector.value = constrain(moveVideoBySelector.value, 0.1, null);
-  preferedSpeedSelector.value = constrain(preferedSpeedSelector.value, 0.1, 5);
-  changeSpeedBySelector.value = constrain(changeSpeedBySelector.value, 0.1, null);
-
-  let values = parseSpeedValues(speedValuesInput.value);
-  let constrainedValues = values.map((v) => constrain(v, 0.1, 5));
-  let uniqueValues = [...new Set(constrainedValues)];
-  let sorted = uniqueValues.sort();
-  let valuesText = sorted.join(',');
-  speedValuesInput.value = valuesText;
-}
-
-function constrain(value, min, max) {
-  if(min != null && value < min) value = min;
-  if(max != null && value > max) value = max;
-  return value;
-}
-
-const regexp = /(\d+\.\d+|\d+)/g;
-function parseSpeedValues(string) {
-  var values = [];
-  let matches = [...string.matchAll(regexp)];
-  for (match of matches){
-    let value = match[0];
-    values.push(value);
-  }
-  return values;
 }
 
 function saveSettings() {
@@ -55,7 +27,38 @@ function saveSettings() {
   storeValue({"preferedSpeed": preferedSpeedSelector.value});
 }
 
-// "saveSettings"
+function ensureProperValues() {
+  const min = 0.1;
+  const max = 5;
+  moveVideoBySelector.value = constrain(moveVideoBySelector.value, min, null);
+  changeSpeedBySelector.value = constrain(changeSpeedBySelector.value, min, null);
+  preferedSpeedSelector.value = constrain(preferedSpeedSelector.value, min, max);
+  speedValuesInput.value = constrainSpeedValues(speedValuesInput.value, min, max);
+}
+
+function constrain(value, min, max) {
+  if(min != null && value < min) value = min;
+  if(max != null && value > max) value = max;
+  return value;
+}
+
+function constrainSpeedValues(values, min, max) {
+  let parsedValues = parseSpeedValues(values);
+  let constrainedValues = parsedValues.map((v) => constrain(v, min, max));
+  let uniqueValues = [...new Set(constrainedValues)];
+  let sorted = uniqueValues.sort();
+  return sorted.join(',');
+}
+
+function parseSpeedValues(string) {
+  var values = [];
+  let matches = [...string.matchAll(numberExp)];
+  for (match of matches){
+    let value = match[0];
+    values.push(value);
+  }
+  return values;
+}
 
 function storeValue(kvPair) {
   chrome.storage.local.set(kvPair);
