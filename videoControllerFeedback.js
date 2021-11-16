52a27a2d5c93150f9eb6b2af8daa69516b94bd2a
Rager Florian <florianrager90@gmail.com>
2021-11-16 09:41:53 +0100

    Show play/pause Icon when command is issued

--- videoControllerFeedback.js
+++ videoControllerFeedback.js
@@ -1,7 +1,7 @@
-console.log("feedback script loaded");
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  var cmd = { id: request.cmd };
+  var video = document.getElementsByTagName("video")[0];
 
-let func = () => {
-  const video = document.getElementsByTagName("video")[0];
   var vidHeight = video.offsetHeight;
   var vidWidth = video.offsetWidth;
   var rect = video.getBoundingClientRect();
@@ -16,44 +16,54 @@ let func = () => {
   div.style.left = xpos + vidWidth / 2 - 50 + "px";
   div.style.top = ypos + vidHeight / 2 - 50 + "px";
 
+  var imgFile = null;
+
+  switch (cmd.id) {
+    case "play-pause":
+      imgFile = video.paused ? "images/play.svg" : "images/pause.svg";
+      break;
+    case "move-video-forward":
+      imgFile = "images/right-arrow.svg";
+      break;
+    case "move-video-back":
+      imgFile = "images/left-arrow.svg";
+      break;
+    case "toggle-speed":
+      console.log("Show current speed");
+      break;
+    case "increase-speed":
+      console.log("Show current speed");
+      break;
+    case "decrease-speed":
+      console.log("Show current speed");
+      break;
+    case "reset-speed":
+      console.log("Show current speed");
+      break;
+    case "toggle-loop":
+      console.log("Show loop state icon");
+      break;
+    case "set-loop-start":
+      console.log("Show loop state icon");
+      break;
+    case "set-loop-end":
+      console.log("Show loop state icon");
+      break;
+    case "remove-loop":
+      imgFile = "images/breakloop.svg";
+      break;
+  }
+  if (imgFile == null) return;
+
   var img = new Image();
-  img.id = "myImage";
-  img.src = chrome.runtime.getURL("images/loop.svg");
+  img.src = chrome.runtime.getURL(imgFile);
   img.width = "140";
   img.height = "140";
 
   div.appendChild(img);
   document.body.appendChild(div);
-};
 
-chrome.runtime.onMessage.addListener((request, sender, response) => {
-  func();
-
-  var cmd = { id: request.cmd };
-  var video = document.getElementsByTagName("video")[0];
-
-  switch (cmd.id) {
-    case "play-pause":
-      break;
-    case "move-video-forward":
-      break;
-    case "move-video-back":
-      break;
-    case "toggle-speed":
-      break;
-    case "increase-speed":
-      break;
-    case "decrease-speed":
-      break;
-    case "reset-speed":
-      break;
-    case "toggle-loop":
-      break;
-    case "set-loop-start":
-      break;
-    case "set-loop-end":
-      break;
-    case "remove-loop":
-      break;
-  }
+  setTimeout(() => {
+    document.body.removeChild(div);
+  }, 300);
 });
