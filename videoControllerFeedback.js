091f9787f745b75421168973aa8632157acc37e1
Rager Florian <florianrager90@gmail.com>
2021-11-16 13:58:37 +0100

    Properly center smaller icons in popup

--- videoControllerFeedback.js
+++ videoControllerFeedback.js
@@ -6,13 +6,13 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
 
     switch (cmd.id) {
       case "play-pause":
-        popup = imagePopup(videoState.paused ? "images/pause.svg" : "images/play.svg");
+        popup = imagePopup(videoState.paused ? "images/pause.svg" : "images/play.svg", 60, 60);
         break;
       case "move-video-forward":
-        popup = imagePopup("images/right-arrow.svg");
+        popup = imagePopup("images/right-arrow.svg", 60, 60);
         break;
       case "move-video-back":
-        popup = imagePopup("images/left-arrow.svg");
+        popup = imagePopup("images/left-arrow.svg", 60, 60);
         break;
       case "toggle-speed":
       case "increase-speed":
@@ -50,24 +50,34 @@ function getLoopStateImage(loop) {
   }
 }
 
+const iconHeight = 100;
+const iconWidth = 100;
+const popupHeight = iconHeight + 40;
+const popupWidth = iconWidth + 40;
+
 function textPopup(text) {
   var container = popupContainer();
 
   var p = document.createElement("h1");
   p.textContent = text;
+  p.width = iconWidth;
+  p.height= iconHeight;
   p.style.fontSize = 20;
 
   container.appendChild(p);
   return container;
 }
 
-function imagePopup(imagePath) {
+function imagePopup(imagePath, width = 100, height = 100) {
   var container = popupContainer();
 
   var img = new Image();
   img.src = chrome.runtime.getURL(imagePath);
-  img.width = "140";
-  img.height = "140";
+  img.width = width;
+  img.height = height;
+  img.style.justifyContent = "center";
+
+  setPopupChildMargin(img, width, height);
 
   container.appendChild(img);
   return container;
@@ -77,12 +87,27 @@ function popupContainer() {
   var video = document.getElementsByTagName("video")[0];
 
   var div = document.createElement("div");
-  div.width = "140";
-  div.height = "140";
-  div.style.backgroundColor = "darkgray";
+  div.width = popupWidth;
+  div.height = popupHeight;
+  div.style.padding = "20px";
+  div.style.borderRadius = "50px";
+  div.style.backgroundColor = "#828282";
+  div.style.opacity = "0.8";
   centerInHost(div, video);
 
   return div;
+}
+
+function setPopupChildMargin(child, width, height) {
+  if(width != iconWidth || height != iconHeight) {
+    var verticalMargin = (iconHeight - height) / 2 + "px";
+    child.style.marginTop = verticalMargin;
+    child.style.marginBottom = verticalMargin;
+
+    var horizontalMargin = (iconWidth - width) / 2 + "px";
+    child.style.marginLeft = horizontalMargin;
+    child.style.marginRight = horizontalMargin;
+  }
 }
 
 function centerInHost(element, host) {
